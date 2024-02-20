8ccfbd3fdac5659db3aaa2adc93f0926d62fbd8c
Video <videogamesm12@gmail.com>
2024-02-20 05:39:42 -0700

    Fixes TopicReferenceCountWME not being decompiled correctly

--- facade/characters/wmedef/TopicReferenceCountWME.java
+++ facade/characters/wmedef/TopicReferenceCountWME.java
@@ -17,64 +17,11 @@ public class TopicReferenceCountWME extends WME
     }
     
     public void setTopicID(final int p0) {
-        // 
-        // This method could not be decompiled.
-        // 
-        // Original Bytecode:
-        // 
-        //     3: aload_3        
-        //     4: monitorexit    
-        //     5: athrow         
-        //     6: aload_0        
-        //     7: dup            
-        //     8: astore_3       
-        //     9: monitorenter   
-        //    10: aload_0        
-        //    11: getfield        facade/characters/wmedef/TopicReferenceCountWME.topicID:I
-        //    14: istore_2       
-        //    15: aload_0        
-        //    16: iload_1        
-        //    17: putfield        facade/characters/wmedef/TopicReferenceCountWME.topicID:I
-        //    20: aload_3        
-        //    21: monitorexit    
-        //    22: aload_0        
-        //    23: getfield        facade/characters/wmedef/TopicReferenceCountWME.__support:Ljava/beans/PropertyChangeSupport;
-        //    26: ldc             "topicID"
-        //    28: iload_2        
-        //    29: iload_1        
-        //    30: invokevirtual   java/beans/PropertyChangeSupport.firePropertyChange:(Ljava/lang/String;II)V
-        //    33: return         
-        //    Exceptions:
-        //  Try           Handler
-        //  Start  End    Start  End    Type
-        //  -----  -----  -----  -----  ----
-        //  3      5      3      6      Any
-        //  10     22     3      6      Any
-        // 
-        // The error that occurred was:
-        // 
-        // java.util.ConcurrentModificationException
-        //     at java.util.ArrayList$Itr.checkForComodification(Unknown Source)
-        //     at java.util.ArrayList$Itr.next(Unknown Source)
-        //     at com.strobel.decompiler.ast.AstBuilder.convertLocalVariables(AstBuilder.java:2863)
-        //     at com.strobel.decompiler.ast.AstBuilder.performStackAnalysis(AstBuilder.java:2445)
-        //     at com.strobel.decompiler.ast.AstBuilder.build(AstBuilder.java:108)
-        //     at com.strobel.decompiler.languages.java.ast.AstMethodBodyBuilder.createMethodBody(AstMethodBodyBuilder.java:211)
-        //     at com.strobel.decompiler.languages.java.ast.AstMethodBodyBuilder.createMethodBody(AstMethodBodyBuilder.java:99)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createMethodBody(AstBuilder.java:782)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createMethod(AstBuilder.java:675)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.addTypeMembers(AstBuilder.java:552)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createTypeCore(AstBuilder.java:519)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createTypeNoCache(AstBuilder.java:161)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createType(AstBuilder.java:150)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.addType(AstBuilder.java:125)
-        //     at com.strobel.decompiler.languages.java.JavaLanguage.buildAst(JavaLanguage.java:71)
-        //     at com.strobel.decompiler.languages.java.JavaLanguage.decompileType(JavaLanguage.java:59)
-        //     at com.strobel.decompiler.DecompilerDriver.decompileType(DecompilerDriver.java:330)
-        //     at com.strobel.decompiler.DecompilerDriver.decompileJar(DecompilerDriver.java:251)
-        //     at com.strobel.decompiler.DecompilerDriver.main(DecompilerDriver.java:126)
-        // 
-        throw new IllegalStateException("An error occurred while decompiling this method.");
+        synchronized (this) {
+            int i = this.topicID;
+            this.topicID = paramInt;
+            this.__support.firePropertyChange("topicID", i, paramInt);
+        }
     }
     
     public synchronized int getCount() {
@@ -82,64 +29,11 @@ public class TopicReferenceCountWME extends WME
     }
     
     public void setCount(final int p0) {
-        // 
-        // This method could not be decompiled.
-        // 
-        // Original Bytecode:
-        // 
-        //     3: aload_3        
-        //     4: monitorexit    
-        //     5: athrow         
-        //     6: aload_0        
-        //     7: dup            
-        //     8: astore_3       
-        //     9: monitorenter   
-        //    10: aload_0        
-        //    11: getfield        facade/characters/wmedef/TopicReferenceCountWME.count:I
-        //    14: istore_2       
-        //    15: aload_0        
-        //    16: iload_1        
-        //    17: putfield        facade/characters/wmedef/TopicReferenceCountWME.count:I
-        //    20: aload_3        
-        //    21: monitorexit    
-        //    22: aload_0        
-        //    23: getfield        facade/characters/wmedef/TopicReferenceCountWME.__support:Ljava/beans/PropertyChangeSupport;
-        //    26: ldc             "count"
-        //    28: iload_2        
-        //    29: iload_1        
-        //    30: invokevirtual   java/beans/PropertyChangeSupport.firePropertyChange:(Ljava/lang/String;II)V
-        //    33: return         
-        //    Exceptions:
-        //  Try           Handler
-        //  Start  End    Start  End    Type
-        //  -----  -----  -----  -----  ----
-        //  3      5      3      6      Any
-        //  10     22     3      6      Any
-        // 
-        // The error that occurred was:
-        // 
-        // java.util.ConcurrentModificationException
-        //     at java.util.ArrayList$Itr.checkForComodification(Unknown Source)
-        //     at java.util.ArrayList$Itr.next(Unknown Source)
-        //     at com.strobel.decompiler.ast.AstBuilder.convertLocalVariables(AstBuilder.java:2863)
-        //     at com.strobel.decompiler.ast.AstBuilder.performStackAnalysis(AstBuilder.java:2445)
-        //     at com.strobel.decompiler.ast.AstBuilder.build(AstBuilder.java:108)
-        //     at com.strobel.decompiler.languages.java.ast.AstMethodBodyBuilder.createMethodBody(AstMethodBodyBuilder.java:211)
-        //     at com.strobel.decompiler.languages.java.ast.AstMethodBodyBuilder.createMethodBody(AstMethodBodyBuilder.java:99)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createMethodBody(AstBuilder.java:782)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createMethod(AstBuilder.java:675)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.addTypeMembers(AstBuilder.java:552)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createTypeCore(AstBuilder.java:519)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createTypeNoCache(AstBuilder.java:161)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createType(AstBuilder.java:150)
-        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.addType(AstBuilder.java:125)
-        //     at com.strobel.decompiler.languages.java.JavaLanguage.buildAst(JavaLanguage.java:71)
-        //     at com.strobel.decompiler.languages.java.JavaLanguage.decompileType(JavaLanguage.java:59)
-        //     at com.strobel.decompiler.DecompilerDriver.decompileType(DecompilerDriver.java:330)
-        //     at com.strobel.decompiler.DecompilerDriver.decompileJar(DecompilerDriver.java:251)
-        //     at com.strobel.decompiler.DecompilerDriver.main(DecompilerDriver.java:126)
-        // 
-        throw new IllegalStateException("An error occurred while decompiling this method.");
+        synchronized (this) {
+          int i = this.count;
+          this.count = paramInt;
+          this.__support.firePropertyChange("count", i, paramInt);
+        }
     }
     
     public TopicReferenceCountWME(final int topicID) {
